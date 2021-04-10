23bfb4c995df9d6a8cab5703259a4750ca530cc6
Ben <>
2021-04-10 06:32:12 +0000

    Bdd build

--- lycee/models.py
+++ lycee/models.py
@@ -4,50 +4,52 @@ from django.db import models
 class Cursus(models.Model):
  name = models.CharField(
     verbose_name="Cursus Name :",
-    max_length=255,
-    blank=True,
-    null=False,
-    default='',
+    max_length=50,
+    blank=False,
+    null=True,
+    default='aucun',
     help_text='Nom du cursus :'
   )
  year_from_bac = models.SmallIntegerField(
     verbose_name="Year from BAC :",
-    blank=True,
-    null=False,
+    blank=False,
+    null=True,
     default=0,
     help_text='Annee depuis le bac :'
   )
  scholar_year = models.CharField(
-    verbose_name="Year from BAC :",
-    blank=True,
-    null=False,
+    verbose_name="Scholar year :",
+    max_length=9,
+    blank=False,
     default='0000-0001',
-    help_text='Annee depuis le bac :'
+    null=True,
+    help_text='Annee scolaire :'
   )
-  def __str__(self):
+ def __str__(self):
     return '{} {}'.format(self.name, self.scholar_year)
 
-class Students(models.Model):
-  first_name = models.CharField(
+class Student(models.Model):
+ first_name = models.CharField(
+    verbose_name="Prenom :",
     max_length=50,
-    blank=False,
+    blank=False,      
     null=False
   )
-  birth_date = models.DateField(
+ birth_date = models.DateField(
     verbose_name="Date of birth :",
     blank=False,
     null=False,
     default=''
   )
-  last_name = models.CharField(
-    verbose_name="Date of birth :",
+ last_name = models.CharField(
+    verbose_name="Last Name :",
     max_length=50,
     blank=False,
     null=False,
     default='???',
-    help_text='date de naissance de l etudiant'
+    help_text='nom de famille de l etudiant'
   )
-  phone_number = models.CharField(
+ phone_number = models.CharField(
     verbose_name="Phone number :",
     max_length=10,
     blank=True,
@@ -55,15 +57,15 @@ class Students(models.Model):
     default='0123456789',
     help_text='numero de telephone'
   )
-  email = models.CharField(
-    verbose_name="Email :",
+ email = models.CharField(
+   verbose_name="Email :",
     max_length=255,
     blank=False,
     null=False,
     default='???@???.com',
     help_text='Courriel'
   )
-  comments = models.CharField(
+ comments = models.CharField(
     verbose_name="Comments :",
     max_length=255,
     blank=True,
@@ -71,30 +73,10 @@ class Students(models.Model):
     default='',
     help_text='Commentaires :'
   )
-cursus = models.ForeignKey(
-  Cursus,
-  on_delete=models.CASCADE,
-  null=True,
-  related_name="student_name"
-)
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
+ cursus = models.ForeignKey(
+    Cursus,
+    on_delete=models.CASCADE,
+    null=True
+  )
+ def __str__(self):
+   return '{} {}'.format(self.first_name, self.last_name)
