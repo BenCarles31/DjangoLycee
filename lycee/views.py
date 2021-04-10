b5ce1a7a6029700427f729e3721002dce5573867
Ben <>
2021-04-10 13:00:00 +0000

    Import Template master + css

--- lycee/views.py
+++ lycee/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from .models import Cursus
+from django.template import loader
+
+def index(request):
+  result_list = Cursus.objects.order_by('name')
+  #charge le template
+  
+  template = loader.get_template('lycee/index.html')
+  
+  #Contexte
+  context = {
+    'liste' : result_list,
+  }
+  #return HttpResponse(template.render(context, request))
+  return render( request , 'lycee/index.html', context)
+
+
+def detail(request, cursus_id):
+  resp = 'result for cursus {}'.format(cursus_id)
+  return HttpResponse(resp)
+
